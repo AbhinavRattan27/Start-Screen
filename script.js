919ea47213e2f101faaee0b64ac67387fe14762a
AbhinavRattan27 <83867719+AbhinavRattan27@users.noreply.github.com>
2022-01-23 13:43:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,79 @@
-function button1js(){
-    window.location.replace("https://abhinavrattan27.github.io/The-CHANCE-Game/");
+var mine = Math.floor(Math.random() * 10) + 1;
+
+function lfthnd(){
+  var mine = Math.floor(Math.random() * 10) + 1;
+  document.getElementById('varh5').innerHTML= mine;
+  if (mine= 1){
+    document.getElementById('hekkin').innerHTML="left";
+    }
+  if (mine = 3){
+    document.getElementById('hekkin').innerHTML="left";
+    }
+  if (mine = 5){
+    document.getElementById('hekkin').innerHTML="left";
+    }
+  if (mine = 7){
+    document.getElementById('hekkin').innerHTML="left";
+    }
+  if (mine = 9){
+    document.getElementById('hekkin').innerHTML="left";
+    }
+    if (mine = 2){
+      document.getElementById('hekkin').innerHTML="right";
+      }
+      if (mine = 4){
+      document.getElementById('hekkin').innerHTML="right";
+      }
+      if (mine = 6){
+      document.getElementById('hekkin').innerHTML="right";
+      }
+      if (mine = 8){
+      document.getElementById('hekkin').innerHTML="right";
+      }
+      if (mine = 10){
+      document.getElementById('hekkin').innerHTML="right";
+      }
+  }
+function rhthnd(){
+  var mine = Math.floor(Math.random() * 10) + 1;
+  document.getElementById("varh5").innerHTML= mine;
+    if (mine = 2){
+    document.getElementById("hekkin").innerHTML="right";
+    }
+    if (mine = 4){
+    document.getElementById("hekkin").innerHTML="right";
+    }
+    if (mine = 6){
+    document.getElementById("hekkin").innerHTML="right";
+    }
+    if (mine = 8){
+    document.getElementById("hekkin").innerHTML="right";
+    }
+    if (mine = 10){
+    document.getElementById("hekkin").innerHTML="right";
+    }
+    if (mine= 1){
+      document.getElementById("hekkin").innerHTML="left";
+      }
+    if (mine = 3){
+      document.getElementById("hekkin").innerHTML="left";
+      }
+    if (mine = 5){
+      document.getElementById("hekkin").innerHTML="left";
+      }
+    if (mine = 7){
+      document.getElementById("hekkin").innerHTML="left";
+      }
+    if (mine = 9){
+      document.getElementById("hekkin").innerHTML="left";
+      }
+  
 }
 
-function button2js(){
-    document.getElementById("bye").innerHTML="Bye!"
-    window.location.replace("https://www.google.com/");
-}
+
+
+   
+
+
+
+  
